e247aa956265ff2abce99c289bf6a3b12a167f61
LeraiOg <86412800+LeraiOg@users.noreply.github.com>
2023-05-02 18:45:43 +0100

    Update Main.lua
    There are now 5 rounds,
    Numbers are properly converted

--- Main.lua
+++ Main.lua
@@ -1,8 +1,50 @@
 local correctAnswer ="Berlin"
-print "Enter capital City of Germany"
+local correctAnswer2 = 4
+local correctAnswer3 = 13
+local correctAnswer4 = 1998
+local correctAnswer5 = 32
+print "What is the capital city of Germany"
 input =  io.read()
 if(input == correctAnswer) then
  print("You got it right")
+ print("You've advanced to Round 2")
 else 
   print(correctAnswer .. " Was the correct answer")
+end
+print("What is 2+2?")
+input = io.read()
+local number = (tonumber(input))
+if (number == correctAnswer2) then
+ print("You got it right")
+ print("You've advanced to Round 3")
+else 
+  print(correctAnswer2 .. " Was the correct answer")
+end
+print("How much are there in a baker's dozen?")
+input = io.read()
+local number = (tonumber(input))
+if (number == correctAnswer3) then
+ print("You got it right")
+ print("You've advanced to Round 4")
+else 
+  print(correctAnswer3 .. " Was the correct answer")
+end
+print("What year was the Good Friday agreement signed?")
+input = io.read()
+local number = (tonumber(input))
+if (number == correctAnswer4) then
+ print("You got it right")
+ print("You've advanced to Round 5!")
+ print("This is the last round!")
+else 
+  print(correctAnswer4 .. " Was the correct answer")
+end
+print("How many nations participate in the world cup?")
+input = io.read()
+local number = (tonumber(input))
+if (number == correctAnswer5) then
+ print("You got it right")
+ print("Congratulations you've won the game!")
+else 
+  print(correctAnswer5 .. " Was the correct answer")
 end
